c63bed03bc1ac5ead604d61df6de24b9ff015991
hsikelias <plekishsai@gmail.com>
2025-08-21 15:02:36 -0400

    Add examples of random, list, and loop usage

--- day2.py
+++ day2.py
@@ -1,6 +1,57 @@
 # Randomisaion and Python Lists
 
 import random
+"""
+rando = random.random()
+print(rando)"""
 
-rando = random.randint(1,10)
-print(rando)
+
+"""
+random_float = random.uniform(5,6)
+print(random_float) 
+
+"""
+"""
+            #0        #1       #2        #3
+fruits = ["apple", "corn", "orange", "tomato"]
+            #-4      #-3      #-2         #-1   
+print(fruits[-4]) """
+
+"""
+Lottery Simulator
+
+Generate 6 random numbers between 1 and 49 (no repeats).
+
+Store them in a list and print them sorted.
+
+"""
+"""
+# convering a list into string
+
+ran_num = random.sample(range(1,50),6)
+result = ", ".join(str(item) for item in ran_num)
+print(result)
+
+"""
+
+# for loop
+"""
+fruits = ["apple", "peach", "pear"]
+
+for fruit in fruits:
+    print(fruit)
+    print(fruit + "pear")
+    print(fruit)
+
+"""
+"""
+while True:
+    for item in fruits:
+        print(fruits) 
+"""
+
+#loops with range
+
+for number in range(5,11):
+    print(number)
+
